1922280b4d28329c812b3104e9337b1055297429
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-09-27 08:27:21 +0000

    backend commit

--- src/components/Songs.jsx
+++
@@ -1,6 +0,0 @@
-import { Song } from "./Song";
-export const Songs = ({allsongs,fn})=>{
-    return(<>
-        {allsongs.map((currentSong,index)=>{return(<Song fn={fn}key={index} song={currentSong}/>)})}   
-        </>)
-}
